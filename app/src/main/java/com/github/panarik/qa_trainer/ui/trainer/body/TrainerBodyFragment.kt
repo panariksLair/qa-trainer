53e2013937ba68e1da9710d51295c157be267674
panarik <panariks@gmail.com>
2024-06-19 12:53:07 +0200

    Add test views to trainer body layout.

--- app/src/main/java/com/github/panarik/qa_trainer/ui/trainer/body/TrainerBodyFragment.kt
+++ app/src/main/java/com/github/panarik/qa_trainer/ui/trainer/body/TrainerBodyFragment.kt
@@ -6,28 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.github.panarik.qa_trainer.R
 import com.github.panarik.qa_trainer.databinding.FragmentTrainerBodyBinding
 
 class TrainerBodyFragment : Fragment() {
 
     private var binding: FragmentTrainerBodyBinding? = null
-    private val viewModel by lazy {
-        ViewModelProvider(this)[TrainerBodyViewModel::class.java].init(
-            this
-        )
+    private val model: TrainerBodyViewModel by lazy {
+        ViewModelProvider(this)[TrainerBodyViewModel::class.java].init(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_trainer_body, container, false)
+    override fun onCreateView(inf: LayoutInflater, con: ViewGroup?, state: Bundle?): View? {
+        binding = FragmentTrainerBodyBinding.inflate(inf, con, false)
+        return binding!!.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel
+    override fun onViewCreated(viewRoot: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(viewRoot, savedInstanceState)
+        val layout = binding?.trainerBodyLayout
+        model.getTrainerItems().forEachIndexed { index, view ->
+            layout?.addView(view, index)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/github/panarik/qa_trainer/ui/trainer/body/TrainerBodyViewModel.kt
+++ app/src/main/java/com/github/panarik/qa_trainer/ui/trainer/body/TrainerBodyViewModel.kt
@@ -1,13 +1,25 @@
 package com.github.panarik.qa_trainer.ui.trainer.body
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.ViewModel
+import com.github.panarik.qa_trainer.R
 
 class TrainerBodyViewModel : ViewModel() {
 
     lateinit var fragment: TrainerBodyFragment
 
-    fun init(fragment: TrainerBodyFragment) {
+    fun init(fragment: TrainerBodyFragment): TrainerBodyViewModel {
         this.fragment = fragment
+        return this
     }
+
+    fun getTrainerItems(): List<View> =
+        listOf(
+            TextView(fragment.context).apply { text = "Text 1" },
+            ImageView(fragment.context).apply { setImageResource(R.drawable.trainer_item_testrail_logo) },
+            TextView(fragment.context).apply { text = "Text 2" }
+        )
 
 }
